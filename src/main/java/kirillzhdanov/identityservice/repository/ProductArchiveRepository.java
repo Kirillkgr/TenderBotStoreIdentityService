3cfa124b8ec47cd3e87c3e8c3a8f1db35df0d520
Kirill <RillGd@gmail.com>
2025-09-21 13:39:11 +0200

    Добавлен архив товаров и дополнительный функционал для работы с товаром. Также написаны тесты для фиксации рабочего функционала. Изменен фазовый класс для запуска тестового контейнера(Пропустил файл).

--- src/main/java/kirillzhdanov/identityservice/repository/ProductArchiveRepository.java
+++ src/main/java/kirillzhdanov/identityservice/repository/ProductArchiveRepository.java
@@ -3,5 +3,10 @@ package kirillzhdanov.identityservice.repository;
 import kirillzhdanov.identityservice.model.product.ProductArchive;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.time.LocalDateTime;
+
 public interface ProductArchiveRepository extends JpaRepository<ProductArchive, Long> {
+    List<ProductArchive> findByBrandId(Long brandId);
+    long deleteByArchivedAtBefore(LocalDateTime threshold);
 }
